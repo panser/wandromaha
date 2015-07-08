824acd98ada9bb7dde5a80fe25e93fc23aa10e33
panser <gostroy@gmail.com>
2015-07-09 00:43:00 +0300

    working copy ...

--- panser/citationGenerator/angularjs/filters.js
+++ panser/citationGenerator/angularjs/filters.js
@@ -2,3 +2,18 @@
 
 var citationGeneratorFilters = angular.module('citationGeneratorFilters', []);
 
+citationGeneratorFilters.filter("generateCitation", function () {
+    // value - данные для которых применяется фильтр
+    // toUpper - аргумент передаваемый фильтру
+    return function (value, styleName) {
+        // проверка переменной value на наличие строки
+        if (angular.isObject(value)) {
+            if (styleName == 'style1') {
+                var processedValue = value.field1 + ' ' + value.field2;
+                return processedValue;
+            }
+        } else {
+            return value;
+        }
+    };
+});
